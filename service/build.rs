e2182b4a589dd6a476c6178feeb13dc78f15a0dc
fanghao <yangfanghao@gmail.com>
2022-11-14 14:07:25 -0500

    The previous version has issues in duplicate workspaces since the confusion in build dependency. It is fixed here.

--- service/build.rs
+++ service/build.rs
@@ -21,7 +21,7 @@ fn main() {
     let sdk_dir = env::var("SGX_SDK").unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
     let is_sim = env::var("SGX_MODE").unwrap_or_else(|_| "HW".to_string());
 
-    println!("cargo:rustc-link-search=native=../lib");
+    println!("cargo:rustc-link-search=native=./lib");
     println!("cargo:rustc-link-lib=static=Enclave_u");
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
