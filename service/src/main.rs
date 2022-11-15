1906738f23e00c827725c135c2937c817617d009
fanghao <yangfanghao@gmail.com>
2022-11-15 16:37:06 -0500

    Add listener framework to monitor TREX data events and scan key pieces for timed-release.

--- service/src/main.rs
+++ service/src/main.rs
@@ -16,13 +16,22 @@
 mod config;
 
 use log::debug;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+// substrate modules
+use sp_core::{sr25519, H256 as Hash, Decode};
+use frame_system::EventRecord;
 use substrate_api_client::rpc::WsRpcClient;
-use substrate_api_client::{Api, AssetTipExtrinsicParams};
+use substrate_api_client::{Api, utils::FromHexString, AssetTipExtrinsicParams};
 
-use sp_runtime::generic::SignedBlock as SignedBlockG;
-use trex_runtime::Block;
-type SignedBlock = SignedBlockG<Block>;
+// use sp_runtime::generic::SignedBlock as SignedBlockG;
+// use trex_runtime::Block;
+// type SignedBlock = SignedBlockG<Block>;
 
+// trex modules
+use trex_runtime::RuntimeEvent;
+use pallet_trex::Event as TrexEvent;
 // local modules
 use config::Config;
 
@@ -32,4 +41,51 @@ fn main(){
     let config_f = std::fs::File::open("config.yml").expect("Could not open file.");
     let config: Config = serde_yaml::from_reader(config_f).expect("Could not read values.");
     debug!("Node server address: {}", config.node_ip);
+    // TODO: Get account ID of current key-holder node.
+    // TODO: Send remote attestation as ext to the trex network.
+    // Spawn a thread to listen to the TREX data event.
+    let event_url = config.node_ip.clone();
+    let mut handlers = Vec::new();
+    handlers.push(thread::spawn(move || {
+        // Listen to TREXDataSent events.
+        let client = WsRpcClient::new(&event_url);
+        let api = Api::<sr25519::Pair, _, AssetTipExtrinsicParams>::new(client).unwrap();
+        println!("Subscribe to TREX events");
+        let (events_in, events_out) = channel();
+        api.subscribe_events(events_in).unwrap();
+        loop {
+            let event_str = events_out.recv().unwrap();
+            let _unhex = Vec::from_hex(event_str).unwrap();
+            let mut _er_enc = _unhex.as_slice();
+            let events = Vec::<EventRecord<RuntimeEvent, Hash>>::decode(&mut _er_enc).unwrap();
+            // match event with trex event
+            for event in &events {
+                debug!("decoded: {:?} {:?}", event.phase, event.event);
+                match &event.event {
+                    // match to trex events.
+                    RuntimeEvent::Trex(te) => {
+                        debug!(">>>>>>>>>> TREX event: {:?}", te);
+                        // match trex data sent event.
+                        match &te {
+                            TrexEvent::TREXDataSent(id, byte_data) => {
+                                // TODO: deserialize TREX struct data and check key pieces.
+                                todo!();
+                            },
+                            _ => {
+                                debug!("ignoring unsupported TREX event");
+                            },
+                        }
+                    },
+                    _ => debug!("ignoring unsupported module event: {:?}", event.event),
+                }
+            }
+            // wait 100 ms for next iteration
+            thread::sleep(Duration::from_millis(100));
+        }
+    }));
+    // TODO: check the enclave and release expired key pieces.
+    // join threads.
+    for handler in handlers {
+        handler.join().expect("The thread being joined has panicked");
+    }
 }
