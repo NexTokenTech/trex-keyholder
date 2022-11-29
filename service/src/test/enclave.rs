58ba01586950f5547b6221b9843febd127a6b4fe
kaisuki <1103720832@qq.com>
2022-11-29 13:05:11 +0800

    add some unit tests

--- service/src/test/enclave.rs
+++ service/src/test/enclave.rs
@@ -18,8 +18,16 @@ use crate::enclave::{
 	api::{enclave_init, get_shielding_pubkey},
 	ffi,
 };
+use aes_gcm::{
+	aead::{rand_core::RngCore, Aead, KeyInit, OsRng},
+	Aes256Gcm, Nonce,
+};
 use log::info;
 use sgx_types::sgx_status_t;
+use sgx_urts::SgxEnclave;
+use trex_primitives::ShieldedKey;
+use crate::utils::node_rpc::get_shielding_key;
+use super::primitive::consts::{TEST_CIPHER,TEST_KEY_PIECE};
 
 #[test]
 fn shielding_key_decryption() {
@@ -53,5 +61,81 @@ fn shielding_key_decryption() {
 		);
 	};
 	assert_eq!(res, 0, "Decrypted message does not match original!");
+	assert_eq!(retval, sgx_status_t::SGX_SUCCESS, "SGX ECall is not successful!")
+}
+
+#[test]
+fn aes_key_generate_encryption_works() {
+	// get aes key
+	let mut key_slice = [0u8; KEY_SIZE];
+	let nonce_slice = AES_NONCE;
+	OsRng.fill_bytes(&mut key_slice);
+	let cipher =
+		Aes256Gcm::new_from_slice(&key_slice).expect("Random key slice does not match the size!");
+	let aes_nonce = Nonce::from_slice(nonce_slice);
+	// create cipher text
+	let ciphertext = cipher.encrypt(aes_nonce, b"a test cipher text".as_ref()).unwrap();
+	assert_eq!(ciphertext, TEST_CIPHER, "Cipher text does not match original!");
+}
+
+#[test]
+fn aes_key_derive_decryption_works(){
+	let mut key_piece = TEST_KEY_PIECE;
+	let (key_slice, nonce_slice) = key_piece.split_at_mut(KEY_SIZE);
+	let ciphertext = TEST_CIPHER;
+	let cipher = Aes256Gcm::new_from_slice(&key_slice)
+		.expect("Random key slice does not match the size!");
+	let aes_nonce = Nonce::from_slice(nonce_slice);
+	let decrypted_msg_slice = cipher.decrypt(aes_nonce,ciphertext.as_ref()).unwrap();
+	let decrypted_msg = String::from_utf8(decrypted_msg_slice.to_vec()).unwrap_or("".to_string());
+	assert_eq!(decrypted_msg,"a test cipher text","Decrypted message does not match original!");
+}
+
+#[test]
+fn aes_key_release_time_hash_works(){
+	let enclave = match enclave_init() {
+		Ok(r) => {
+			info!("[+] Init Enclave Successful {}!", r.geteid());
+			r
+		},
+		Err(x) => {
+			info!("[-] Init Enclave Failed {}!", x.as_str());
+			return
+		},
+	};
+	// get ras pubkey and enclave account id, will insert into ShieldedKey.
+	let (rsa_pubkey, tee_account_id) = get_shielding_key(&config).unwrap();
+	let mut key_piece = TEST_KEY_PIECE;
+	// generate hash of Sha256PrivateKeyTime which contains key_piece and release_time
+	let release_time = release_time();
+	let key_time = Sha256PrivateKeyTime {
+		aes_private_key: key_piece.clone().to_vec(),
+		timestamp: release_time.clone(),
+	};
+	let key_time_hash = key_time.hash();
+	// construct key hash struct for shielding
+	let key_hash =
+		Sha256PrivateKeyHash { aes_private_key: key_piece.clone().to_vec(), hash: key_time_hash };
+	let key_hash_encode = key_hash.encode();
+	// shielding key hash struct
+	let mut cipher_private_key: Vec<u8> = Vec::new();
+	rsa_pubkey
+		.encrypt_buffer(&key_hash_encode, &mut cipher_private_key)
+		.expect("Cannot shield key pieces!");
+	// construct key_pieces
+	let key: ShieldedKey = cipher_private_key;
+	let mut retval = sgx_status_t::SGX_SUCCESS;
+	let mut res: u8 = 1;
+	unsafe {
+		ffi::test_key_piece(
+			enclave.geteid(),
+			&mut retval,
+			key.as_ptr(),
+			key.len() as u32,
+			release_time,
+			&mut res,
+		);
+	};
+	assert_eq!(res, 1, "Decrypted key time hash does not match original!");
 	assert_eq!(retval, sgx_status_t::SGX_SUCCESS, "SGX ECall is not successful!")
 }
